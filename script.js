41632e9453895842ea8580070a2032f4c791ffca
NikhileshS <nikhileshs2004@gmail.com>
2024-03-24 15:57:42 +0530

    Improved the design and fixed some more bugs

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Variable initialization //
 let no1 = 0;
 let no2 = 0;
 let operator = 0;
 let disptot = 0;
+let displayno = 0;
 
 function add(a,b){
     return a+b;
@@ -110,6 +112,7 @@ button0.addEventListener('click',() => {
 // Addition Function //
 const addi = document.querySelector(".addi")
 addi.addEventListener('click',() => {
+    if(disptot === 0 || isNaN(disptot)){
     if(arr.length === 0 ){
         alert('Please enter a number')
         return display.textContent = 0;
@@ -118,14 +121,28 @@ addi.addEventListener('click',() => {
     arr.push('+');
     no1 = arr2.join('');
     arr2 = [];
-    operator = '+'
+    operator = '+';
     return display.textContent = arr.join('');
     }
-})
+}
+else{
+    disptot = display.textContent
+    arr =[]
+    arr.push(disptot);
+    arr.push('+');
+    no1 = disptot;
+    arr2 = [];
+    operator = '+';
+    disptot = 0;
+    return display.textContent = arr.join('');
+}
+}
+)
 
 // Subtraction function // 
 const subtra = document.querySelector(".subtra")
 subtra.addEventListener('click',() => {
+    if(disptot === 0 || isNaN(disptot)){
     if(arr.length === 0 ){
         alert('Please enter a number')
         return display.textContent = 0;
@@ -133,15 +150,27 @@ subtra.addEventListener('click',() => {
     else{
     arr.push('-');
     no1 = arr2.join('');
-    arr2 = [];
     operator = '-'
     return display.textContent = arr.join('');
     }
+}
+else{
+    disptot = display.textContent
+    arr =[]
+    arr.push(disptot);
+    arr.push('-');
+    no1 = disptot;
+    arr2=[];
+    operator = '-';
+    disptot = 0;
+    return display.textContent = arr.join('');
+}
 })
 
 // Multiplication function // 
 const multi = document.querySelector(".multi")
 multi.addEventListener('click',() => {
+    if(disptot === 0 || isNaN(disptot)){
     if(arr.length === 0 ){
         alert('Please enter a number')
         return display.textContent = 0;
@@ -153,11 +182,24 @@ multi.addEventListener('click',() => {
     operator = '*'
     return display.textContent = arr.join('');
     }
+}
+else{
+    disptot = display.textContent
+    arr =[]
+    arr.push(disptot);
+    arr.push('*');
+    no1 = disptot;
+    arr2=[];
+    operator = '*';
+    disptot = 0;
+    return display.textContent = arr.join('');
+}
 })
 
 // Divide function //
 const divide = document.querySelector(".divide")
 divide.addEventListener('click',() => {
+    if(disptot === 0 || isNaN(disptot)){
     if(arr.length === 0 ){
         alert('Please enter a number')
         return display.textContent = 0;
@@ -169,6 +211,18 @@ divide.addEventListener('click',() => {
     operator = '/'
     return display.textContent = arr.join('');
     }
+}
+else{
+    disptot = display.textContent
+    arr =[]
+    arr.push(disptot);
+    arr.push('/');
+    no1 = disptot;
+    arr2=[];
+    operator = '/';
+    disptot = 0;
+    return display.textContent = arr.join('');
+}
 })
 
 // Clear function //
@@ -188,17 +242,31 @@ equals.addEventListener('click',()=>{
     no2 = arr2.join('');
     if(no1 === 0 ){
         disptot = no2;
+        displayno = disptot;
+        disptot = 0;
         no2 = 0;
         arr2 = [];
     }
     else{
         disptot = operate(parseInt(no1),operator,parseInt(no2));
+        if(isNaN(disptot)){
+            alert('Invalid format')
+            displayno =0
+            disptot = 0;
+            no1=0;
+            no2=0;
+            arr = [];
+            arr2 = [];
+        }
+        else{
+        displayno = disptot;
         no1=0;
         no2=0;
         arr=[];
         arr2=[];
+        }
     }
-    return display.textContent = disptot;
+    return display.textContent = displayno;
 })
 
 
