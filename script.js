78045051c3457b8c4e90460d9ee10b51bf457fdf
NikhileshS <nikhileshs2004@gmail.com>
2024-03-25 07:00:17 +0530

    Reduced LoC and fixed minor bugs

--- script.js
+++ script.js
@@ -48,65 +48,14 @@ let arr2 = [];
 let oper;
 
 // Wiring up each HTML buttons to do their respective actions //
-const button1 = document.querySelector(".one");
-button1.addEventListener('click',() => {
-    arr.push(1);
-    arr2.push(1);
-    return display.textContent = arr.join('');
-})
-const button2 = document.querySelector(".two");
-button2.addEventListener('click',() => {
-    arr.push(2);
-    arr2.push(2);
-    return display.textContent = arr.join('');
-})
-const button3 = document.querySelector(".three");
-button3.addEventListener('click',() => {
-    arr.push(3);
-    arr2.push(3);
-    return display.textContent = arr.join('');
-})
-const button4 = document.querySelector(".four");
-button4.addEventListener('click',() => {
-    arr.push(4);
-    arr2.push(4);
-    return display.textContent = arr.join('');
-})
-const button5 = document.querySelector(".five");
-button5.addEventListener('click',() => {
-    arr.push(5);
-    arr2.push(5);
-    return display.textContent = arr.join('');
-})
-const button6 = document.querySelector(".six");
-button6.addEventListener('click',() => {
-    arr.push(6);
-    arr2.push(6);
-    return display.textContent = arr.join('');
-})
-const button7 = document.querySelector(".seven");
-button7.addEventListener('click',() => {
-    arr.push(7);
-    arr2.push(7);
-    return display.textContent = arr.join('');
-})
-const button8 = document.querySelector(".eight");
-button8.addEventListener('click',() => {
-    arr.push(8);
-    arr2.push(8);
-    return display.textContent = arr.join('');
-})
-const button9 = document.querySelector(".nine");
-button9.addEventListener('click',() => {
-    arr.push(9);
-    arr2.push(9);
-    return display.textContent = arr.join('');
-})
-const button0 = document.querySelector(".zero");
-button0.addEventListener('click',() => {
-    arr.push(0);
-    arr2.push(0);
-    return display.textContent = arr.join('');
+const btn = document.querySelectorAll('.btn');
+btn.forEach(btn => {
+    btn.addEventListener('click',() =>{
+        let num = btn.textContent;
+        arr.push(num);
+        arr2.push(num);
+        return display.textContent = arr.join('');
+    })
 })
 
 // Addition Function //
